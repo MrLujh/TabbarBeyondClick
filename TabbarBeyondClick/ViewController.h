1b8daf441291c103f474966a1f22f8f0c34f67c1
lujh <287929070@qq.com>
2017-04-17 22:01:31 +0800

    Initial Commit

---
+++ TabbarBeyondClick/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TabbarBeyondClick
+//
+//  Created by 卢家浩 on 2017/4/17.
+//  Copyright © 2017年 lujh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TabbarBeyondClick/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TabbarBeyondClick
+//
+//  Created by 卢家浩 on 2017/4/17.
+//  Copyright © 2017年 lujh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
